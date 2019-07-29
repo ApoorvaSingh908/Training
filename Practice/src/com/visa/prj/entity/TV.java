9846eb182f3ba6b5c2b8a7216bab7feecd25c22f
apoorva908 <apoorvasingh.908@gmail.com>
2019-07-29 11:54:07 +0000

    day 1 EOD

---
+++ Practice/src/com/visa/prj/entity/TV.java
@@ -0,0 +1,24 @@
+package com.visa.prj.entity;
+
+public class TV extends Product {
+	private String connectivity;
+
+	public TV() {
+		
+	}
+
+	public TV(int id, String name, double price, String connectivity) {
+		super(id, name, price);
+		this.connectivity = connectivity;
+	}
+
+	public String getConnectivity() {
+		return connectivity;
+	}
+
+	public void setConnectivity(String connectivity) {
+		this.connectivity = connectivity;
+	}
+	
+	
+}
